bcc89df28cf61c1c8c477ae0d5ea806d4ebac6d8
eschmidt <eschmidt@mail.uni-paderborn.de>
2020-03-22 22:15:14 +0100

    position slots can now have multiple values

--- src/app/models/metadata.ts
+++ src/app/models/metadata.ts
@@ -1,19 +1,34 @@
 export class Metadata {
-  brand: string = 'Unknown brand';
-  modelFilename: string = '';
-  price: number = 1.00;
-  leftSlot: number = 0;
-  rightSlot: number = 0;
-  upperSlot: number = 0;
+  brand = 'Unknown brand';
+  modelFilename = '';
+  price = 1.00;
+  leftSlot: number[] = [0];
+  rightSlot: number[] = [0];
+  upperSlot: number[] = [0];
 
   constructor(data?: any) {
-    if (data == null) return;
+    if (data == null) { return; }
 
-    if (data.brand) this.brand = data.brand;
-    if (data.price != null && +data.price >= 0) this.price = +data.price;
-    if (data.leftSlot != null && +data.leftSlot >= -1) this.leftSlot = +data.leftSlot;
-    if (data.rightSlot != null && +data.rightSlot >= -1) this.rightSlot = +data.rightSlot;
-    if (data.upperSlot != null && +data.upperSlot >= -1) this.upperSlot = +data.upperSlot;
-    if (data.modelFilename) this.modelFilename = data.modelFilename;
+    if (data.brand) { this.brand = data.brand; }
+    if (data.price != null && +data.price >= 0) { this.price = +data.price; }
+    if (data.leftSlot != null && data.leftSlot.length > 0) {
+      const input = data.leftSlot.map(val => +val);
+      this.leftSlot = input;
+      if (input.includes(0)) { this.leftSlot = [0]; }
+      if (input.includes(-1)) { this.leftSlot = [-1]; }
+    }
+    if (data.rightSlot != null && data.rightSlot.length > 0) {
+      const input = data.rightSlot.map(val => +val);
+      this.rightSlot = input;
+      if (input.includes(0)) { this.rightSlot = [0]; }
+      if (input.includes(-1)) { this.rightSlot = [-1]; }
+    }
+    if (data.upperSlot != null && data.upperSlot.length > 0) {
+      const input = data.upperSlot.map(val => +val);
+      this.upperSlot = input;
+      if (input.includes(0)) { this.upperSlot = [0]; }
+      if (input.includes(-1)) { this.upperSlot = [-1]; }
+    }
+    if (data.modelFilename) { this.modelFilename = data.modelFilename; }
   }
 }
